16fa1ee13d75ae8217530d1bc46ed72b0a41fd02
Abhishekfm <62643405+Abhishekfm@users.noreply.github.com>
2023-02-08 23:59:49 +0530

    update url

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export function App(){
     const [carTotal, setCarTotal] = useState("")
     const [carAvailable, setCarAvailable] = useState("")
     const [ carUrl, setCarUrl] = useState("")
-    const BaseUrl = process.env.BASE_URL
+    const BaseUrl = "https://online-booking-car-rentals-backend.vercel.app"
     return(
         <>
         <MyContext.Provider value={[globalVariable, setGlobalVariable, user, setUser, carName, setCarName, carTotal, setCarTotal, carAvailable, setCarAvailable, carUrl, setCarUrl]}>
