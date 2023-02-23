8a6837ac792e484e5ef7b7da3abe9ff67f159f7c
Abhishekfm <62643405+Abhishekfm@users.noreply.github.com>
2023-02-23 14:41:38 +0530

    For Better Result

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -10,6 +10,63 @@ export function NavBar(props){
     const [user, setUser] = useContext(MyContext)
     const [ myRole, setMyRole] = useState("")
     const [resp, setResp] = useState(false)
+    const [showModal, setShowModal] = useState(false)
+    const modalContent = (
+      <div>
+          <div
+          className="justify-center items-center flex overflow-x-hidden backdrop-blur-sm overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+        >
+          <div className="relative w-auto my-6 mx-auto max-w-[320px] 2xs:max-w-sm xs:max-w-lg sm:max-w-xl md:max-w-3xl">
+            {/*content*/}
+            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              {/*header*/}
+              <div className="flex items-start justify-between p-1 sm:p-3 border-b border-solid border-slate-200 rounded-t">
+                <h3 className="text-xl 2md:text-2xl font-semibold">
+                For Better Results 
+                </h3>
+                <button
+                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-40 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  onClick={() => closeMe()}
+                >
+                  <span className=" text-[#000] text-[29px] block focus:outline-none">
+                  ×
+                  </span>
+                </button>
+              </div>
+              {/*body*/}
+              <div className="relative p-3 2md:p-6 gap-2 flex flex-col flex-auto">
+                  <p className="my-1 md:my-4 text-slate-500 text-md 2md:text-lg leading-relaxed">
+                  Search in Country India And State Uttar Pradesh And City Noida
+                  </p>
+                  <em className="text-red-600 text-[14px] 2md:text-lg">Note:- For better Use Of Application</em>
+              </div>
+              {/*footer*/}
+              <div className="flex items-center justify-end p-2 border-t border-solid border-slate-200 rounded-b">
+                <button
+                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={() => closeMe()}
+                >
+                  Close
+                </button>
+                <button
+                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={() => closeMe()}
+                >
+                  Okay
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+
+      </div>
+  );
+  function closeMe(){
+    setShowModal(false)
+  }
     async function logMeOut(){
       try {
         const res = await axios.post(`${props.BaseUrl}/auth/logout`,
@@ -51,12 +108,14 @@ export function NavBar(props){
     }
     return(
         <>
+        {showModal? modalContent:""}
         <nav className="bg-slate-800 text-white py-3 px-4 flex justify-between items-center">
             <Link className="text-lg font-semibold tracking-wide uppercase" to="/">DriveMe</Link>
             <div className="hidden md:flex">
               <Link className="mx-4 text-base font-medium tracking-wide uppercase" to="/" >Home</Link>
               {myRole === "ADMIN" || myRole === "USER"?<Link className="mx-4 text-base font-medium tracking-wide uppercase" to="/order" >Order</Link>:""}
               {myRole === "ADMIN"?<Link className="mx-4 text-base font-medium tracking-wide uppercase" to="/users" >Users</Link>:""}
+              <p onClick={()=>{setShowModal(true)}} className="mx-4 text-base font-medium tracking-wide uppercase">Available IN</p>
               {myRole === "ADMIN"?<Link className="mx-4 text-base font-medium tracking-wide uppercase" to="/admin" >Admin Controller</Link>:""}
               {myRole === "ADMIN"?<Link className="mx-4 text-base font-medium tracking-wide uppercase" to="/allorder" >All Order</Link>:""}
               {myRole === "ADMIN" || myRole === "USER"?<Link onClick={()=>{logMeOut()}} className="mx-4 text-base font-medium tracking-wide uppercase" to="/login" >LogOut</Link>:<Link className="mx-4 text-base font-medium tracking-wide uppercase" to="/login" >LogIn</Link>}
@@ -71,6 +130,7 @@ export function NavBar(props){
                   <Link className=" text-base font-medium px-4 py-1 w-full hover:bg-slate-200 text-[#000] tracking-wide uppercase" to="/" >Home</Link>
                   {myRole === "ADMIN" || myRole === "USER"?<Link className=" px-4 py-1 hover:bg-slate-200 text-base text-[#000] font-medium tracking-wide uppercase" to="/order" >Order</Link>:""}
                   {myRole === "ADMIN"?<Link className="px-4 py-1 text-base font-medium hover:bg-slate-200 tracking-wide text-[#000] uppercase" to="/users" >Users</Link>:""}
+                  <p onClick={()=>{setShowModal(true)}} className="mx-4 text-base font-medium tracking-wide uppercase">Available IN</p>
                   {myRole === "ADMIN"?<Link className="px-4 py-1 text-base font-medium hover:bg-slate-200 tracking-wide text-[#000] uppercase" to="/admin" >Admin Controller</Link>:""}
                   {myRole === "ADMIN"?<Link className="px-4 py-1 text-base font-medium hover:bg-slate-200 tracking-wide text-[#000] uppercase" to="/allorder" >All Order</Link>:""}
                   {myRole === "ADMIN" || myRole === "USER"?<Link onClick={()=>{logMeOut()}} className=" px-4 py-1 text-[#000] hover:bg-slate-200 text-base font-medium tracking-wide uppercase" to="/login" >LogOut</Link>:<Link className=" px-4 py-1 hover:bg-slate-200 text-[#000] text-base font-medium tracking-wide uppercase" to="/login" >LogIn</Link>}
